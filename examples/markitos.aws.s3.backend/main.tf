fd5bb82f97882150ca6e910893998f927c913d55
markitos <markitos.info@gmail.com>
2023-11-11 06:58:45 +0100

    - starting examples, howto use modules

---
+++ examples/markitos.aws.s3.backend/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = "1.6.2"
+
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = local.environment_data.AWS_ACCESS_KEY
+  secret_key = local.environment_data.AWS_SECRET_ACCESS_KEY
+  region     = local.environment_data.AWS_REGION
+}
+
+locals {
+  environment_data = { for tuple in regexall("(.*)=(.*)", file("./.env")) : tuple[0] => tuple[1] }
+}
+
+module "markitos_backend" {
+  source                    = "../../markitos.aws.s3.backend"
+  backend_env_file          = "./.env"
+  project_name              = "markitos_terramods"
+  backend_s3_region         = "eu-west-1"
+  backend_s3_bucket_encrypt = true
+  backend_s3_bucket_key     = "markitos_terramods/terraform/states"
+  backend_s3_bucket_id      = "markitos_terramods.terraform.state"
+  backend_dynamodb_table    = "markitos_terramods-main-terraform-lock"
+}
